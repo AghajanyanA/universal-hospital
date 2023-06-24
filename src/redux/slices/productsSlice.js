16a30be39a5fd83cb89742b9f7603fc752993d38
AghajanyanA <astara@null.net>
2023-06-25 00:54:54 +0400

    refactoring & minor fixes

--- src/redux/slices/productsSlice.js
+++ src/redux/slices/productsSlice.js
@@ -152,6 +152,32 @@ const initialState = {
                 image: bint,
                 description: 'Բինտեր, թանզիֆե մանրէազերծված բժշկական անձեռոցիկներ, մանրէազերծված և ոչ մանրէազերծված բժշկական վիրակապեր արտադրող;Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting'
             },
+            {
+                id: 6,
+                name: 'բժշկական անձեռոցիկ  ՍՏԵՐԻԼ (ինդիվիդուալ)',
+                sterile: true,
+                layers: 10,
+                pieces: 12,
+                dimensions: {
+                    y: 8.5,
+                    x: 7.5
+                },
+                image: bint,
+                description: 'Բինտեր, թանզիֆե մանրէազերծված բժշկական անձեռոցիկներ, մանրէազերծված և ոչ մանրէազերծված բժշկական վիրակապեր արտադրող;Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting'
+            },
+            {
+                id: 7,
+                name: 'բժշկական անձեռոցիկ ոչ ՍՏԵՐԻԼ',
+                sterile: false,
+                layers: 8,
+                pieces: 10,
+                dimensions: {
+                    y: 10,
+                    x: 15
+                },
+                image: bint,
+                description: 'Բինտեր, թանզիֆե մանրէազերծված բժշկական անձեռոցիկներ, մանրէազերծված և ոչ մանրէազերծված բժշկական վիրակապեր արտադրող;Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting'
+            },
             
         ],
         top: [
--- src/router/ReactRouter.jsx
+++ src/router/ReactRouter.jsx
@@ -3,14 +3,21 @@ import { Main } from "../Components/Pages/0Main/Main"
 import { ContactShort } from "../Components/Pages/0Main/4Contact/ContactShort"
 import { Partners } from "../Components/Pages/4Partners/Partners"
 import { About } from "../Components/Pages/1About/About"
+import { Products } from "../Components/Pages/2Products/Products"
+import { Bandages } from "../Components/Pages/2Products/0Bandages/Bandages"
 
 export const ReactRouter = () => (
         <Routes>
             <Route path="/" element={ <Main /> } />
-            <Route path="/about" element={ <About /> } />
-            <Route path="/products" element={ <>gh</> } />
-            <Route path="/services" element={ <>gh</> } />
-            <Route path="/partners" element={ <Partners />} />
-            <Route path="/contact" element={ <ContactShort /> } />
+            <Route path="about" element={ <About /> } />
+            <Route path="products" element={ <Products /> }>
+                <Route path="bandages" element={ <Bandages /> } />
+                <Route path="tanzif" element={ <>tanzif</>} />
+                <Route path="napkins" element={ <>napkins</>} />
+                <Route path="top" element={ <>top</>} />
+            </Route>
+            <Route path="services" element={ <>gh</> } />
+            <Route path="partners" element={ <Partners />} />
+            <Route path="contact" element={ <ContactShort /> } />
         </Routes>
 )
