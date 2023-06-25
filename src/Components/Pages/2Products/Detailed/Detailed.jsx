5d12835026c833008b10f44e5e65af4d39a5de6b
AghajanyanA <astara@null.net>
2023-06-25 13:24:59 +0400

    Detailed page Component initial

---
+++ src/Components/Pages/2Products/Detailed/Detailed.jsx
@@ -0,0 +1,16 @@
+import { useParams } from 'react-router-dom'
+import s from './Detailed.module.css'
+import { useSelector } from 'react-redux'
+import { productsSelector } from '../../../../helpers/selectors'
+
+export const Detailed = () => {
+
+    const {productID, category} = useParams()
+    const products = useSelector(productsSelector)
+    const product = products[category].find(ob => ob.id === +productID)
+
+
+    return <div className={s.wrapper}>
+        
+        </div>
+}
